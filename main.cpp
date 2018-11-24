5abc02614cad46d2d487870dea2ce6e00a0d81c2
Blake Higgins <blake.higgins36@gmail.com>
2018-11-24 17:37:58 -0600

    Write how main will use morse class

--- main.cpp
+++ main.cpp
@@ -3,7 +3,37 @@
 #include "print.h"
 
 int main() {
-    console::print("hello world");
+    //Know the Secret Code
+    morse::secretCode('file.txt');
 
-    return 0;
+    //Receive Message
+    string received = ".... . .-.. .-.. ---";
+    console::print("You've Got a Mail!", received);
+    console::print("Decode It? (y/n)");
+    char seeIt = console::input();
+
+    if(seeIt == 'n')
+        return morse::exit();
+
+    //Decode Message
+    string decoded = morse::decode()
+    console::print("They Said", decoded);
+
+    //Reply
+    console::print("Reply Back? (y/n)");
+    char reply = console::input();
+
+    if(reply == 'n')
+        return morse::exit();
+
+    console::print("Message: ");
+    string response = console::input();
+
+    //Encode Response
+    string message = morse::encode(response);
+    console::print("Received Message: ", message);
+
+    //Send Message & Exit
+    morse::send(message);
+    return morse::exit();
 }
